25b85176e61183050babd2ab7be914a3f517d494
Dulaj Nawodya <dulajnawodya5@gmail.com>
2025-07-13 15:11:56 +0530

    add removed drug routes

--- backend1/routes/api.php
+++ backend1/routes/api.php
@@ -19,3 +19,10 @@ Route::get('/j', [ConnectedEntityController::class, 'index']);
 Route::post('/entities', [ConnectedEntityController::class, 'store']);
 Route::put('/entities/{entity}', [ConnectedEntityController::class, 'update']);
 Route::delete('/entities/{entity}', [ConnectedEntityController::class, 'destroy']);
+
+Route::post('/drugs', [DrugController::class, 'store']);
+Route::get('/drug', [DrugController::class, 'index']);
+Route::put('/drug/{drug}', [DrugController::class, 'update']);
+Route::delete('/drug/{drug}', [DrugController::class, 'destroy']);
+
+// Route::delete('/drugs/{drug}', [DrugController::class, 'destroy']);
